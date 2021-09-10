aa93597504eb1b68322d8829a218780c09556629
Frois <frois.dev@gmail.com>
2021-09-10 08:37:26 -0300

    create routing structure

---
+++ src/Routes.tsx
@@ -0,0 +1,20 @@
+import { Switch, Route } from "react-router-dom";
+import Details from "./views/pages/Details";
+import Home from "./views/pages/Home";
+
+const Routes = () => {
+  return (
+    <div>
+      <Switch>
+        <Route path="/pokemon/:id">
+          <Details/>
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+export default Routes;
---
+++ src/views/pages/Details.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Details = () => (
+    <div>
+        <p>oi</p>
+    </div>
+)
+
+export default Details;
