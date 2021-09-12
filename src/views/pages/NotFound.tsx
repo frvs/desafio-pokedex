aa8d0adb0f4001a88e98c475380a87c387b883c9
Frois <frois.dev@gmail.com>
2021-09-12 16:27:46 -0300

    add 404 page redirect

--- src/views/pages/NotFound.tsx
+++ src/views/pages/NotFound.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router';
+import styled from 'styled-components';
 
-const NotFound = (): JSX.Element => (
-  <div>
-    <p>404</p>
-  </div>
-);
+const Main = styled.div`
+  width: 80%;
+  margin: 10% auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const NotFound = (): JSX.Element => {
+  const history = useHistory();
+
+  useEffect(() => {
+    setTimeout(() => {
+      history.push('/');
+    }, 5000);
+  }, []);
+  return (
+    <Main>
+      <img src={process.env.PUBLIC_URL + '/notfound.png'} style={{ width: `300px` }} />
+      <h1>Oops, aconteceu um erro :(</h1>
+      <h3>Você será redirecionado em alguns instantes...</h3>
+    </Main>
+  );
+};
 
 export default NotFound;
