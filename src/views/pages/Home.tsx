d69c3e79b3ff58c6311efd1c2acf3240c3f161ab
Frois <frois.dev@gmail.com>
2021-09-12 15:29:11 -0300

    add styled component

--- src/views/pages/Home.tsx
+++ src/views/pages/Home.tsx
@@ -3,6 +3,29 @@ import { useHistory } from 'react-router';
 import { usePokemons } from '../../services/api';
 import PokemonCard from '../components/PokemonCard';
 import Spinner from '../components/Spinner';
+import styled from 'styled-components';
+
+const Main = styled.div`
+  width: 70%;
+  margin: 0 auto;
+`;
+
+const PokemonsWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(1, 1fr);
+
+  @media (min-width: 320px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media (min-width: 481px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  @media (min-width: 769px) {
+    grid-template-columns: repeat(4, 1fr);
+  }
+`;
 
 const Home = (): JSX.Element => {
   const { pokemons, isLoading, isError } = usePokemons();
@@ -10,15 +33,16 @@ const Home = (): JSX.Element => {
 
   if (isLoading) return <Spinner />;
   if (isError) history.push('/404');
-
   return (
-    <div style={{ display: `grid`, gridTemplateColumns: `1fr 1fr 1fr 1fr` }}>
-      {pokemons.pokemon.map((pokemon) => (
-        <div key={pokemon.id}>
-          <PokemonCard id={pokemon.id} name={pokemon.name} img={pokemon.img} type={pokemon.type} />
-        </div>
-      ))}
-    </div>
+    <Main>
+      <PokemonsWrapper>
+        {pokemons.pokemon.map((pokemon) => (
+          <div key={pokemon.id}>
+            <PokemonCard num={pokemon.num} name={pokemon.name} img={pokemon.img} type={pokemon.type} />
+          </div>
+        ))}
+      </PokemonsWrapper>
+    </Main>
   );
 };
 
